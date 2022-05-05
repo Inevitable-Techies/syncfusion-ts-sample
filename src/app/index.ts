7adc501abbad7723d9f7152f64a55ef2c1637573
RagunathSukumaran <ragunath.sf3421@gmail.com>
2022-05-05 14:58:21 +0530

    TS sample of Date Picker component.

---
+++ src/app/index.ts
@@ -0,0 +1,31 @@
+/**
+ * Replace the ts code of the component in thebelow code.
+ */
+
+import { DatePicker } from '@syncfusion/ej2-calendars';
+/**
+ * Default DatePicker sample
+ */
+
+let datepicker: DatePicker = new DatePicker({
+placeholder: 'Choose a date',
+});
+datepicker.appendTo('#datepicker');
+var button = document.getElementsByClassName('e-date-icon')[0];
+button.addEventListener(
+'touchstart',
+function () {
+    console.log(navigator.userAgent);
+    console.log((window as any).browserDetails);
+    console.log((window as any).browserDetails.isDevice);
+},
+true
+);
+button.addEventListener(
+'click',
+function () {
+    console.log(navigator.userAgent);
+    console.log((window as any).browserDetails.isDevice);
+},
+true
+);
